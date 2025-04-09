3f6f84b22ff3786392cc8c75e5a6fbf9217fb573
Rahul-597 <144713663+Rahul-597@users.noreply.github.com>
2025-04-09 11:26:36 +0000

    change the code

--- rahul.cpp
+++ rahul.cpp
@@ -1,12 +1,9 @@
 #include <iostream>
 #include <cmath>
+using namespace std; 
 
 int main() {
     int n;
-    cin >> n;
-    int length = (n == 0) ? 1 : log10(n) + 1;
-    std::cout << "Number of digits: " << length << std::endl;
-    std::cout << length / 2;
-    bool x = n%2 ==0 ?
-    return 0;
+    cout << "Hello world";
+
 }
